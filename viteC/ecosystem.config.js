cea6984f9ec0ca507ddcc3474a9fec68fd5df520
rishicollinz <rishikesh.b@codingmart.com>
2024-04-22 22:25:51 +0530

    ecosystem modified pm2

--- viteC/ecosystem.config.js
+++ viteC/ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
             script: "/home/ubuntu/jenkins/workspace/Workerjob/viteC/server.js",
             cwd: "/home/ubuntu/jenkins/workspace/Workerjob/viteC",
             watch: false,
-            instances: "3",
+            instances: "1",
             exec_mode: "cluster"
         }
     ]
